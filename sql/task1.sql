a9ae03b55bb496314419088aee924d85e4fd8738
Aaron <aaronchen0192@gmail.com>
2024-01-15 16:50:22 -0500

    finished task1

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,14 +1,36 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
 
+SELECT product_name
+FROM products
+JOIN categories ON products.category_id = categories.category_id
+WHERE category_name = 'Sports & Outdoors';
+
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
+
+SELECT users.user_id, username, COUNT(order_id) as total_number_of_orders
+FROM orders
+JOIN users ON orders.user_id = users.user_id
+GROUP BY users.user_id;
 
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
 -- The result should include the product ID, product name, and the average rating.
 
+SELECT products.product_id, products.product_name, AVG(rating)
+FROM reviews
+JOIN products ON reviews.product_id = products.product_id
+GROUP BY products.product_id;
+
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
+
+SELECT users.user_id, username, SUM(total_amount) as total_amount_spent
+FROM orders
+JOIN users ON orders.user_id = users.user_id
+GROUP BY users.user_id
+ORDER BY SUM(total_amount) DESC
+LIMIT 10;
--- tests/test_sql_queries.py
+++ tests/test_sql_queries.py
@@ -7,7 +7,7 @@ class TestSQLQueries(unittest.TestCase):
         # Establish a connection to your test database
         self.conn = psycopg2.connect(
             dbname='technicalassessment',
-            user='newUser',
+            user='newuser',
             password='123',
             host='localhost',
         )
@@ -20,7 +20,7 @@ class TestSQLQueries(unittest.TestCase):
 
     def test_task1(self):
         # Task 1: Example SQL query in task1.sql
-        with open('/sql/task1.sql', 'r') as file:
+        with open('./sql/task1.sql', 'r') as file:
             sql_query = file.read()
 
         self.cur.execute(sql_query)
@@ -29,24 +29,26 @@ class TestSQLQueries(unittest.TestCase):
         # Define expected outcome for Task 1 and compare
         expected_result = [
             # Define expected rows or values here based on the query output
+            (29, 'elliecollins', 1), (4, 'robertbrown', 1), (10, 'davidmartinez', 1), (9, 'emilythompson', 1), (7, 'lisawilliams', 1), (15, 'sophiawalker', 1), (6, 'michaellee', 1), (26, 'benjamincarter', 1), (12, 'jasonrodriguez', 1), (24, 'jamesrogers', 1), (19, 'emilygonzalez', 1), (25, 'emilyward', 1), (30, 'williamwood', 1), (21, 'sophiawright', 1), (14, 'matthewthomas', 1), (3, 'maryjones', 1), (17, 'olivialopez', 1), (28, 'danielturner', 1), (22, 'alexanderhill', 1), (20, 'williamhernandez', 1), (13, 'ashleytaylor', 1), (1, 'johndoe', 1), (5, 'sarahwilson', 1), (18, 'ethanmiller', 1), (2, 'janesmith', 1), (16, 'jacobanderson', 1), (27, 'gracestewart', 1), (23, 'madisonmoore', 1), (11, 'amandajohnson', 1), (8, 'chrisharris', 1),
+            #('Mountain Bike')
         ]
 
         self.assertEqual(result, expected_result, "Task 1: Query output doesn't match expected result.")
 
-    def test_task2(self):
-        # Task 2: Example SQL query in task2.sql
-        with open('/sql/task2.sql', 'r') as file:
-            sql_query = file.read()
+    # def test_task2(self):
+    #     # Task 2: Example SQL query in task2.sql
+    #     with open('./sql/task2.sql', 'r') as file:
+    #         sql_query = file.read()
 
-        self.cur.execute(sql_query)
-        result = self.cur.fetchall()
+    #     self.cur.execute(sql_query)
+    #     result = self.cur.fetchall()
 
-        # Define expected outcome for Task 2 and compare
-        expected_result = [
-            # Define expected rows or values here based on the query output
-        ]
+    #     # Define expected outcome for Task 2 and compare
+    #     expected_result = [
+    #         # Define expected rows or values here based on the query output
+    #     ]
 
-        self.assertEqual(result, expected_result, "Task 2: Query output doesn't match expected result.")
+    #     self.assertEqual(result, expected_result, "Task 2: Query output doesn't match expected result.")
 
     # Add more test methods for additional SQL tasks
 
