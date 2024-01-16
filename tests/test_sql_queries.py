22939d457c5eb994b4c71b713571e243ded8c7d9
Samyar Farjam <samyarfarjam@outlook.com>
2024-01-16 14:13:08 -0500

    Add testing data and script. Add requirements.txt.

--- tests/test_sql_queries.py
+++ tests/test_sql_queries.py
@@ -1,6 +1,7 @@
 import unittest
 # import psycopg2 
 import mysql.connector
+import pandas as pd
 
 class TestSQLQueries(unittest.TestCase):
     def setUp(self):
@@ -21,48 +22,76 @@ class TestSQLQueries(unittest.TestCase):
 
     # Tests task 1
     def test_task1(self):
-        with open('/sql/task1.sql', 'r') as file:
+        task_num = 1
+        with open(f'/sql/task{task_num}.sql', 'r') as file:
             sql_query = file.read()
 
-        self.cur.execute(sql_query)
-        result = self.cur.fetchall()
+        queries = self.sql_query_separator(sql_query)
 
-        expected_result = [
-            (value1_row1, value2_row1, ...),  # Row 1
-            (value1_row2, value2_row2, ...),  # Row 2
-        ]
+        for i in range(len(queries)):
+            self.cur.execute(queries[i])
+            result = self.cur.fetchall()
 
-        self.assertEqual(result, expected_result, "Task 1: Query output doesn't match expected result.")
+            expected_result = self.expected_results(task_num=task_num, problem= i + 1)
 
+            self.assertEqual(result, expected_result, f"Task {task_num} Problem {i + 1}: Query output doesn't match expected result.")
+        
     # Tests task 2
     def test_task2(self):
-        with open('/sql/task2.sql', 'r') as file:
+        task_num = 2
+        with open(f'/sql/task{task_num}.sql', 'r') as file:
             sql_query = file.read()
 
-        self.cur.execute(sql_query)
-        result = self.cur.fetchall()
+        queries = self.sql_query_separator(sql_query)
 
-        # Define expected outcome for Task 2 and compare
-        expected_result = [
-            # Define expected rows or values here based on the query output
-        ]
+        for i in range(len(queries)):
+            self.cur.execute(queries[i])
+            result = self.cur.fetchall()
 
-        self.assertEqual(result, expected_result, "Task 2: Query output doesn't match expected result.")
+            expected_result = self.expected_results(task_num=task_num, problem= i + 1)
+
+            self.assertEqual(result, expected_result, f"Task {task_num} Problem {i + 1}: Query output doesn't match expected result.")
     
     # Tests task 3
     def test_task3(self):
-        with open('/sql/task3.sql', 'r') as file:
+        task_num = 3
+        with open(f'/sql/task{task_num}.sql', 'r') as file:
             sql_query = file.read()
 
-        self.cur.execute(sql_query)
-        result = self.cur.fetchall()
+        queries = self.sql_query_separator(sql_query)
 
-        # Define expected outcome for Task 3 and compare
-        expected_result = [
-            # Define expected rows or values here based on the query output
-        ]
+        for i in range(len(queries)):
+            self.cur.execute(queries[i])
+            result = self.cur.fetchall()
 
-        self.assertEqual(result, expected_result, "Task 3: Query output doesn't match expected result.")
+            expected_result = self.expected_results(task_num=task_num, problem= i + 1)
+
+            self.assertEqual(result, expected_result, f"Task {task_num} Problem {i + 1}: Query output doesn't match expected result.")
+    
+
+    def sql_query_separator(sql_query):
+        lines = sql_query.split("\n")
+        all_queries = []
+        query = []
+        for line in lines:
+            query.append(line)
+            if line.endswith(";"):
+                all_queries.append("\n".join(query))
+                query = []
+        return all_queries
+    
+    def expected_results(task_num, problem):
+        # Read CSV file into a pandas DataFrame, excluding the header
+        data = pd.read_csv(f'./results/{problem}.csv', header=None)
+
+        # Convert the DataFrame to the test readable format
+        formatted_data = [tuple(row) for row in data.values]
+
+        return formatted_data
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
