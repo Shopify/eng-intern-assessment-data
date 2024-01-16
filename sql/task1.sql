6b43a154a3935619033e005438f825456ead417a
Samyar Farjam <samyarfarjam@outlook.com>
2024-01-16 12:03:27 -0500

    Solve task 1 problem 4

--- sql/task1.sql
+++ sql/task1.sql
@@ -43,3 +43,16 @@ GROUP BY `products`.`product_id`;
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
+-- Purpose: Retrieve information about user with highest total order amounts. We assume orders here
+-- means combination of all orders (considering each user might have had more than one order)
+-- This query joins users and orders, groups data by user_id and sums the total_amount upon aggregation.
+-- At the end orders the records by total_amount_spent in desceding order and only shows first 5 records.
+SELECT
+    `users`.`user_id`,
+    `users`.`username`,
+    SUM(`total_amount`) AS `total_amount_spent`
+FROM `users`
+JOIN `orders` ON `users`.`user_id` = `orders`.`user_id`
+GROUP BY `users`.`user_id`
+ORDER BY `total_amount_spent` DESC
+LIMIT 5;
