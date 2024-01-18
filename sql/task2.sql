b34bb5f5d13654029bbad88ebd7df245cbf2c2bc
leesop10 <leesop10@LAPTOP-HL76IU92>
2024-01-17 20:35:05 -0500

    Task 2 commented and completed. Some problems may have incorrect interpretations. These assumptions are included in comments. The first code line specifies a search_path, which aligns with modified schema.sql file.

--- sql/task2.sql
+++ sql/task2.sql
@@ -1,3 +1,10 @@
+-- NOTE: the following code is in psql
+
+-- NOTE: The search path below is initialized in modified schema.sql code. 
+--       If not tested in this way, please comment out the below line. 
+SET search_path TO onlineShopping;
+
+
 -- Problem 5: Retrieve the products with the highest average rating
 -- Write an SQL query to retrieve the products with the highest average rating.
 -- The result should include the product ID, product name, and the average rating.
@@ -7,11 +14,11 @@
 -- First, a view containing all the average ratings of rated products are created
 DROP VIEW IF EXISTS AvgRatings CASCADE;
 CREATE VIEW AvgRatings AS
-SELECT product_id, product_name, avg(rating) AS average_rating FROM Products NATURAL JOIN Reviews;
+SELECT product_id, product_name, avg(rating) AS average_rating FROM Products NATURAL JOIN Reviews GROUP BY product_id, product_name;
 
 -- All products that have the maximum value for average_rating are included for the final query
 SELECT product_id, product_name, average_rating FROM AvgRatings
-WHERE average_rating >= (SELECT average_rating FROM AvgRatings);
+WHERE average_rating >= ALL(SELECT average_rating FROM AvgRatings);
 
 
 -- Problem 6: Retrieve the users who have made at least one order in each category
@@ -20,12 +27,15 @@ WHERE average_rating >= (SELECT average_rating FROM AvgRatings);
 -- Hint: You may need to use subqueries or joins to solve this problem.
 
 -- First find all the users who lack any category among their orders. (This includes users who have never made an order.)
+--      To do this, subtract the set of all user_ids x category_ids by the actual pairs of user_ids, category_ids. 
+--      The users that are left are those who are missing some user_ids x category_ids, 
+--          meaning they do not satisfy the requirements of the query.
 DROP VIEW IF EXISTS UsersLackingCategories CASCADE;
 CREATE VIEW UsersLackingCategories AS
 SELECT DISTINCT user_id FROM
-(SELECT user_id, category_id FROM Categories, Users)
+((SELECT user_id, category_id FROM Categories, Users)
 EXCEPT 
-(SELECT DISTINCT user_id, category_id FROM Order_Items NATURAL JOIN Products);
+(SELECT DISTINCT user_id, category_id FROM Order_Items NATURAL JOIN Products NATURAL JOIN Orders));
 
 -- Retrieve and return relevant information
 SELECT user_id, username FROM Users WHERE user_id NOT IN UsersLackingCategories;
@@ -47,12 +57,13 @@ FROM Products WHERE product_id NOT IN (SELECT DISTINCT product_id FROM Reviews);
 -- Hint: You may need to use subqueries or window functions to solve this problem.
 
 -- NOTE: I am interpreting the query to mean: users that have at some point more than one order per day for consecutive days
--- Collect all distinct order days associated with users. 
+--          I raised Issue #48 on Github on this interpretation, which is not answered at this time. 
+-- First collect all order days with multiple orders by a user. ("consecutive orders" for a day)
 DROP VIEW IF EXISTS NonUniqueOrderDates CASCADE;
 CREATE VIEW NonUniqueOrderDates AS
 SELECT user_id, order_date FROM Orders GROUP BY user_id, order_date HAVING count(*) > 1;
 
--- Find users with consecutive pairs
-SELECT DISTINCT U.user_id, U.username FROM NonUniqueOrderDates D, NonUniqueOrderDates NATURAL JOIN Users U
-WHERE D.order_date - U.order_date = 1 and D.user_id = U.user_id;
+-- Next, find users with consecutive pairs in the table of days NonUniqueOrderDates
+SELECT DISTINCT U.user_id, U.username FROM NonUniqueOrderDates D1, NonUniqueOrderDates D2 NATURAL JOIN Users U
+WHERE D1.order_date - D2.order_date = 1 and D1.user_id = D2.user_id;
 
