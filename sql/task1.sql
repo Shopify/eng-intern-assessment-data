f882b4ff5e4d348e51e790340a6a001362325f59
daw1882 <47186687+daw1882@users.noreply.github.com>
2024-01-19 17:45:21 -0600

    task2 answers

--- sql/task1.sql
+++ sql/task1.sql
@@ -9,27 +9,24 @@ where category_id = 8;
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
 select u.user_id, u.username, count(o.order_id) as total_orders
-from orders o 
-left join users u 
-	on o.user_id = u.user_id 
+from orders o, users u
+where o.user_id = u.user_id 
 group by o.user_id;
 
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
 -- The result should include the product ID, product name, and the average rating.
 select p.product_id, p.product_name, avg(r.rating) as avg_rating
-from reviews r 
-left join products p 
-	on r.product_id = p.product_id 
+from reviews r, products p 
+where r.product_id = p.product_id 
 group by r.product_id;
 
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
 select u.user_id, u.username, sum(o.total_amount) as total_spent 
-from users u 
-join orders o 
-	on u.user_id = o.user_id 
+from users u, orders o 
+where u.user_id = o.user_id 
 group by u.user_id 
 order by total_spent desc
 limit 5;
