87e98b11c5fc5fb565ad52e06e7f6d48833a371a
Benton Le <78377038+HobbyistProgrammer@users.noreply.github.com>
2024-01-21 13:30:20 -0500

    Completed task2.sql

--- sql/task2.sql
+++ sql/task2.sql
@@ -3,17 +3,59 @@
 -- The result should include the product ID, product name, and the average rating.
 -- Hint: You may need to use subqueries or common table expressions (CTEs) to solve this problem.
 
+WITH PRODUCTRATINGS AS (
+  SELECT p.product_id, p.product_name, AVG(r.rating) AS avg_rating
+  FROM PRODUCTS p
+  INNER JOIN REVIEWS R ON p.product_id = r.product_id
+  GROUP BY p.product_id, p.product_name
+)
+
+SELECT product_id, product_name, avg_rating
+FROM (
+  SELECT *, ROW_NUMBER() OVER (ORDER BY avg_rating DESC) AS "ROW_NUM"
+  FROM PRODUCTRATINGS
+) ranked
+WHERE "ROW_NUM" = 1;
+
 -- Problem 6: Retrieve the users who have made at least one order in each category
 -- Write an SQL query to retrieve the users who have made at least one order in each category.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or joins to solve this problem.
+
+SELECT u.user_id, u.username
+FROM USERS u
+WHERE NOT EXISTS (
+  SELECT c.category_id
+  FROM CATEGORIES C
+  WHERE NOT EXISTS (
+    SELECT p.product_id
+    FROM PRODUCTS p
+    LEFT JOIN ORDERS_ITEMS oi ON p.product_id = oi.product_id
+    LEFT JOIN ORDERS o ON oi.order_id = o.order_id AND o.user_id = u.user_id
+    WHERE c.category_id = p.category_id AND oi.order_id IS NOT NULL
+  )
+);
 
 -- Problem 7: Retrieve the products that have not received any reviews
 -- Write an SQL query to retrieve the products that have not received any reviews.
 -- The result should include the product ID and product name.
 -- Hint: You may need to use subqueries or left joins to solve this problem.
 
+SELECT p.product_id, p.product_name
+FROM PRODUCTS p
+LEFT JOIN  REVIEWS R ON p.product_id = r.product_id
+WHERE r.review_id IS NULL;
+
 -- Problem 8: Retrieve the users who have made consecutive orders on consecutive days
 -- Write an SQL query to retrieve the users who have made consecutive orders on consecutive days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or window functions to solve this problem.
+
+WITH USERCONSECUTIVEORDERS AS (
+  SELECT user_id, order_date, LAG(order_date) OVER (PARTITION BY user_id ORDER BY order_date) AS prev_order
+  FROM ORDERS
+)
+SELECT DISTINCT u.user_id, u.username
+FROM USERS u
+JOIN USERCONSECUTIVEORDERS uco ON u.user_id = uco.user_id
+WHERE uco.order_date = uco.prev_order + 1 ;
