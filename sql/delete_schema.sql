479ddc5a6b677d29cb6fa434a77f90f21dcc2a54
harshkpatel <hkpatel1760@gmail.com>
2024-01-22 10:42:56 -0500

    added database setup files, modified csv for testing, completed task 1 and added tests

---
+++ sql/delete_schema.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS shipping, payments, cart_items, cart,
+    reviews, order_items, orders, users, products, categories;
--- tests/test_sql_queries.py
+++ tests/test_sql_queries.py
@@ -2,15 +2,20 @@ import unittest
 import psycopg2  # Replace with appropriate database connector based on your database
 
 class TestSQLQueries(unittest.TestCase):
+    dbname = 'patelh'
+    user = 'patelh'
+    password = ''
+    host = 'localhost'
+    port = '5431'
 
     def setUp(self):
         # Establish a connection to your test database
         self.conn = psycopg2.connect(
-            dbname='your_dbname',
-            user='your_username',
-            password='your_password',
-            host='your_host',
-            port='your_port'
+            dbname=self.dbname,
+            user=self.user,
+            password=self.password,
+            host=self.host,
+            port=self.port
         )
         self.cur = self.conn.cursor()
 
@@ -21,22 +26,82 @@ class TestSQLQueries(unittest.TestCase):
 
     def test_task1(self):
         # Task 1: Example SQL query in task1.sql
-        with open('/sql/task1.sql', 'r') as file:
+        with open('../sql/task1.sql', 'r') as file:
             sql_query = file.read()
 
         self.cur.execute(sql_query)
         result = self.cur.fetchall()
 
         # Define expected outcome for Task 1 and compare
-        expected_result = [
-            # Define expected rows or values here based on the query output
-        ]
+        # Task 1:
+        # expected_result = [(15, 'Mountain Bike', 'Conquer the trails with this high-performance mountain bike.',
+        #                     1000.00, 8),
+        #                    (16, 'Tennis Racket', 'Take your tennis game to the next level with this '
+        #                                          'professional-grade racket.', 54.00,8)]
+
+        # Task 2:
+        # expected_result = [(1, 'johndoe', 1),
+        #                    (2, 'janesmith', 1),
+        #                    (3, 'maryjones', 1),
+        #                    (4, 'robertbrown', 1),
+        #                    (5, 'sarahwilson', 1),
+        #                    (6, 'michaellee', 1),
+        #                    (7, 'lisawilliams', 1),
+        #                    (8, 'chrisharris', 1),
+        #                    (9, 'emilythompson', 1),
+        #                    (10, 'davidmartinez', 1),
+        #                    (11, 'amandajohnson', 1),
+        #                    (12, 'jasonrodriguez', 1),
+        #                    (13, 'ashleytaylor', 1),
+        #                    (14, 'matthewthomas', 1),
+        #                    (15, 'sophiawalker', 1),
+        #                    (16, 'jacobanderson', 1),
+        #                    (17, 'olivialopez', 1),
+        #                    (18, 'ethanmiller', 1),
+        #                    (19, 'emilygonzalez', 1),
+        #                    (20, 'williamhernandez', 1),
+        #                    (21, 'sophiawright', 1),
+        #                    (22, 'alexanderhill', 1),
+        #                    (23, 'madisonmoore', 1),
+        #                    (24, 'jamesrogers', 1),
+        #                    (25, 'emilyward', 1),
+        #                    (26, 'benjamincarter', 1),
+        #                    (27, 'gracestewart', 1),
+        #                    (28, 'danielturner', 1),
+        #                    (29, 'elliecollins', 1),
+        #                    (30, 'williamwood', 2)]
+
+        # # Task 3:
+        # expected_result = [(1, 'Smartphone X', 4),
+        #                    (2, 'Wireless Headphones', 4.5),
+        #                    (3, 'Laptop Pro', 2.5),
+        #                    (4, 'Smart TV', 4.5),
+        #                    (5, 'Running Shoes', 3.5),
+        #                    (6, 'Designer Dress', 3.5),
+        #                    (7, 'Coffee Maker', 4.5),
+        #                    (8, 'Toaster Oven', 2),
+        #                    (9, 'Action Camera', 4.5),
+        #                    (10, 'Board Game Collection', 2.5),
+        #                    (11, 'Yoga Mat', 3.5),
+        #                    (12, 'Skincare Set', 3.5),
+        #                    (13, 'Vitamin C Supplement', 3.5),
+        #                    (14, 'Weighted Blanket', 3.5),
+        #                    (15, 'Mountain Bike', 5),
+        #                    (16, 'Tennis Racket', 4)]
+
+
+        # Task 4:
+        expected_result = [(30, 'williamwood', 170.00),
+                           (12, 'jasonrodriguez', 160.00),
+                           (4, 'robertbrown', 155.00),
+                           (24, 'jamesrogers', 150.00),
+                           (8, 'chrisharris', 150.00)]
 
         self.assertEqual(result, expected_result, "Task 1: Query output doesn't match expected result.")
 
     def test_task2(self):
         # Task 2: Example SQL query in task2.sql
-        with open('/sql/task2.sql', 'r') as file:
+        with open('../sql/task2.sql', 'r') as file:
             sql_query = file.read()
 
         self.cur.execute(sql_query)
