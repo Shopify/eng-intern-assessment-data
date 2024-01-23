b5655cc24b67ee6052fe3d30cb253366dc748210
ethan-rng <ethan.rong@gmail.com>
2024-01-22 21:33:41 -0500

    finished task 2 and 3

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,8 +1,10 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
-SELECT * 
-FROM Products
-WHERE Products.category_id = (
+SELECT 
+    p.product_id,
+    p.product_name
+FROM Products p
+WHERE p.category_id = (
     -- Retrieve the category ID of the Sports & Outdoors category
     SELECT category_id 
     FROM Categories 
@@ -29,9 +31,10 @@ GROUP BY u.user_id, u.username;
 SELECT 
     p.product_id,
     p.product_name,
-    AVG(r.rating) AS average_rating
+    -- To deal with edge cases where a product has no rating (need to set average_rating to 0)
+    AVG(COALESCE(r.rating, 0)) AS average_rating 
 FROM Products p
--- Left join since, users maybe able to rate products that do not exist, depending
+-- Left join since, users might able to rate products that do not exist, depending
 -- on the way the way the database is setup. We therefore assume, all products are
 -- in the product database
 LEFT JOIN Reviews r ON p.product_id = r.product_id
@@ -42,11 +45,13 @@ GROUP BY p.product_id, p.product_name;
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
 SELECT 
-    Orders.user_id,
-    SUM(Orders.total_amount) AS total_amount
-FROM Orders
-GROUP BY Orders.user_id -- Grouping by the user ID will ensure that the average is calculated for each user
-ORDER BY SUM(Orders.total_amount) DESC -- Ordering by descending order leads to the first 5 largest values
-LIMIT 5;
+    o.user_id,
+    u.username,
+    SUM(o.total_amount) AS total_amount_spent
+FROM Orders o
+LEFT JOIN Users u ON o.user_id = u.user_id
+GROUP BY o.user_id -- Grouping by the user ID will ensure that the average is calculated for each user
+ORDER BY total_amount_spent DESC -- Ordering by descending order leads to the first 5 largest values
+LIMIT 5; -- Limiting the results to the first 5
 
 
--- sql/task3.sql
+++ sql/task3.sql
@@ -2,18 +2,88 @@
 -- Write an SQL query to retrieve the top 3 categories with the highest total sales amount.
 -- The result should include the category ID, category name, and the total sales amount.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+SELECT
+    c.category_id,
+    c.category_name,
+    SUM(oi.quantity * oi.unit_price) AS total_sales
+FROM Categories c
+
+-- Joining the Products and Order_Items tables based on ID to get access to quantity and unit price per order_item
+JOIN Products p ON c.category_id = p.category_id
+JOIN Order_Items oi ON p.product_id = oi.product_id
+GROUP BY c.category_id
+
+-- Limiting the results to the first 3 categories in DESC order by total sales
+ORDER BY total_sales DESC
+LIMIT 3;
+
 
 -- Problem 10: Retrieve the users who have placed orders for all products in the Toys & Games
 -- Write an SQL query to retrieve the users who have placed orders for all products in the Toys & Games
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+SELECT 
+    u.user_id,
+    u.username
+FROM Users u
+
+-- Creating a table with the number of Toys & Games products per user
+JOIN (
+    SELECT
+        o.user_id,
+        COUNT(oi.product_id) AS toys_count
+    FROM Orders o
+    JOIN Order_Items oi ON o.order_id = oi.order_id
+    JOIN Products p ON oi.product_id = p.product_id
+    JOIN Categories c ON p.category_id = c.category_id
+    WHERE c.category_name = 'Toys & Games'
+    GROUP BY o.user_id
+) AS userToysCount ON u.user_id = userToysCount.user_id
+
+-- Only allowing the users with the same number of Toys & Games products as the total number of products
+WHERE userToysCount.toys_count IN (
+    -- Finding the total number of Toys & Games products in the database
+    SELECT 
+        COUNT(p.product_id)
+    FROM Products p
+    JOIN Categories c ON p.category_id = c.category_id
+    WHERE c.category_name = 'Toys & Games'
+)
+-- To Remove Edge Cases Where There Are No Products That Are Toys & Games
+AND userToysCount.toys_count > 0;
+
 
 -- Problem 11: Retrieve the products that have the highest price within each category
 -- Write an SQL query to retrieve the products that have the highest price within each category.
 -- The result should include the product ID, product name, category ID, and price.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+SELECT
+    c.category_id,
+    c.category_name,
+    MAX(p.price) as max_price
+FROM Products p
+JOIN Categories c ON p.category_id = c.category_id
+GROUP BY c.category_id, c.category_name
+ORDER BY c.category_id;
+
 
 -- Problem 12: Retrieve the users who have placed orders on consecutive days for at least 3 days
 -- Write an SQL query to retrieve the users who have placed orders on consecutive days for at least 3 days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+SELECT DISTINCT
+    full_dates.user_id,
+    full_dates.username
+FROM (
+  SELECT 
+        o.user_id, 
+        u.username, 
+        o.order_date,
+        LAG(order_date, 1) OVER (PARTITION BY user_id ORDER BY order_date) as prev_date,
+        LEAD(order_date, 1) OVER (PARTITION BY user_id ORDER BY order_date) as next_date
+    FROM orders o
+    JOIN Users u ON u.user_id = o.user_id
+) AS full_dates
+WHERE 
+    full_dates.prev_date = full_dates.order_date - 1 AND 
+    full_dates.next_date = full_dates.order_date + 1;
