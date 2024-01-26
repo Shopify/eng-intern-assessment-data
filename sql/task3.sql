562be3407b2ff482333279c37f1dbfb1c20fc840
Kevin Kim <kimkevin0612@gmail.com>
2024-01-26 16:11:23 -0500

    Task3 Solutions

--- sql/task3.sql
+++ sql/task3.sql
@@ -17,3 +17,57 @@
 -- Write an SQL query to retrieve the users who have placed orders on consecutive days for at least 3 days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+
+-- Problem 9 Solution
+SELECT TOP 3 T1.category_id, T1.category_name, SUM(T1.sales) as total_sales_amount
+FROM 
+(SELECT T.category_id, T.category_name, O.quantity * O.unit_price AS sales
+FROM 
+(SELECT C.category_id, C.category_name, P.product_id
+FROM Categories C LEFT JOIN Products P
+ON C.category_id = P.category_id) AS T
+LEFT JOIN Order_Items O 
+ON T.product_id = O.product_id 
+GROUP BY O.product_id) AS T1
+GROUP BY T1.category_id
+ORDER BY SUM(T1.sales) DESC;
+
+-- Problem 10 Solution
+
+SELECT DISTINCT T2.user_id, U.username
+FROM
+(SELECT T1.product_id, T1.order_id, O.user_id
+FROM 
+(SELECT T.product_id, O1.order_id 
+FROM 
+(SELECT P.product_id
+FROM Categories C INNER JOIN Products P
+ON C.category_id = P.category_id
+WHERE C.category_id = 5) AS T
+LEFT JOIN Order_Items O1 
+ON T.product_id = O1.product_id) AS T1
+LEFT JOIN Orders O
+ON T1.order_id = O.order_id) AS T2
+LEFT JOIN Users U
+ON T2.user_id = U.user_id
+WHERE T2.user_id IS NOT NULL;
+
+-- Problem 11 Solution
+SELECT DISTINCT T.product_id, T.product_name, T.category_id, T.max_price
+FROM
+(SELECT C.category_id, P.product_id, P.product_name, P.price, MAX(P.price) as max_price
+FROM Categories C LEFT JOIN Products P
+ON C.category_id = P.category_id
+GROUP BY C.category_id, P.product_id) AS T 
+WHERE T.price = T.max_price;
+
+-- Problem 12 Solution
+SELECT DISTINCT T1.user_id, T1.user_name
+FROM
+(SELECT T.user_id, T.user_name, DATEDIFF(day, lag2, lag1) AS diff2, DATEDIFF(day, lag1, T.order_date) AS diff1
+FROM
+(SELECT U.user_id, U.user_name, O.order_date, LAG(O.order_date, 1) OVER (ORDER BY O.order_date ASC) AS lag1, LAG(O.order_date, 2) OVER (ORDER BY O.order_date ASC) AS lag2
+FROM Users U LEFT JOIN Orders O
+ON U.user_id = O.user_id) AS T) AS T1
+WHERE T1.diff2 = 1 AND T1.diff1 = 1;
+
