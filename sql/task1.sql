b6a4b2aaf35c5a7698c53801670e38447d8f8e64
vidyashree-suresh <vidyarao1310@gmail.com>
2024-01-26 18:15:48 -0500

    Tasks done- 1 sub

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,14 +1,67 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
 
+select p.product_id,
+p.product_name 
+from Products as p
+join Categories  as c
+on c.category_id=p.category_id
+where category_name like '%Sports%'; --Category name can be changed and also the filter will be applied wherever the term is found
+
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
+
+select o.user_id,
+u.username, 
+total_orders
+from 
+(
+	select user_id, 
+	count(order_id) as total_orders 
+	from Orders
+	group by user_id
+) as o
+inner  join Users as u
+on u.user_id=o.user_id;
+
+
 
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
 -- The result should include the product ID, product name, and the average rating.
 
+select r.product_id,
+p.product_name,
+average_rating  
+from(
+	select product_id, 
+	round(AVG(rating),2) as average_rating  --Rounding the values of average
+	from Reviews
+	group by product_id
+) as r
+join Products as p
+on p.product_id=r.product_id; 
+
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
+
+
+with rank_cust_spend as
+(
+	select user_id,
+	sum(total_amount) as total_amount_spent,
+	ROW_NUMBER() OVER (ORDER BY  sum(total_amount)  DESC) AS rank_num --assigning row numbers to users 
+    from Orders
+	group by user_id
+)
+
+
+select r.user_id,
+u.username,
+r.total_amount_spent
+from rank_cust_spend as r
+join Users as u
+on r.user_id=u.user_id
+where r.rank_num<6; 
--- sql/task3.sql
+++ sql/task3.sql
@@ -2,18 +2,111 @@
 -- Write an SQL query to retrieve the top 3 categories with the highest total sales amount.
 -- The result should include the category ID, category name, and the total sales amount.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+--Doubtful
+with cat_purchase as
+(
+	select c.category_id,
+	c.category_name, 
+	sum(quantity*unit_price) as total_sales from  Order_Items  as o --Since Order table has total sales, at product level quantity and unit price data is used to calcluate total_sales
+	left join Products as p 
+	on o.product_id=p.product_id
+	left join Categories as c
+	on c.category_id=p.category_id
+	group by c.category_id,c.category_name
+)
+
+select
+category_id,
+category_name,
+total_sales from
+(
+	select
+	category_id,
+	category_name,
+	total_sales,
+	dense_rank() over(order by total_sales desc) as rank_cat -- Using dense rank to resolve the tie and give continuous ranking
+	from cat_purchase
+) as cp
+where rank_cat<4 and category_id is not null;
 
 -- Problem 10: Retrieve the users who have placed orders for all products in the Toys & Games
 -- Write an SQL query to retrieve the users who have placed orders for all products in the Toys & Games
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
 
+with user_puchased_cat as
+(
+	select oi.product_id,
+	o.user_id,
+	username,
+	category_name,
+	p.category_id 
+	from Order_Items as oi
+	join orders as o
+	on oi.order_id=o.order_id
+	left join Products as p
+	on oi.product_id=p.product_id
+	left join Categories as c
+	on c.category_id=p.category_id
+	left join Users as u
+	on o.user_id=u.user_id
+	where (category_name) like  '%Toys & Games%'
+)
+
+--select * from user_puchased_cat
+-- total number of distinct products left should be equal to total number of products with toys & games_id
+  
+
+select distinct user_id, 
+username 
+from user_puchased_cat as u
+where product_id in (
+	select distinct(product_id) 
+	from Products 
+	where category_id=u.category_id);
+
+
 -- Problem 11: Retrieve the products that have the highest price within each category
 -- Write an SQL query to retrieve the products that have the highest price within each category.
 -- The result should include the product ID, product name, category ID, and price.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
 
+select 
+product_id,
+product_name,
+category_id,
+price
+from
+(
+		select
+		p.product_id,
+		product_name,
+		category_id,
+		price,
+		
+		rank() over(partition by category_id order by price desc) as price_rank from  Products as p
+		--on p.product_id=oi.product_id
+) as b
+
+where price_rank=1;
+
+
 -- Problem 12: Retrieve the users who have placed orders on consecutive days for at least 3 days
 -- Write an SQL query to retrieve the users who have placed orders on consecutive days for at least 3 days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+with nextdayorder AS (
+    select
+        user_id,
+        order_date,
+        lead(order_date, 1) over (PARTITION BY user_id ORDER BY order_date) AS next_order,
+        lead(order_date, 2) OVER (PARTITION BY user_id ORDER BY order_date) AS next_order_2 -- lead - 2 column to track third order
+    FROM Orders
+)
+
+select distinct n.user_id,username 
+from nextdayorder as n
+join  Users as u 
+on u.user_id=n.user_id
+where DATEDIFF(day,order_date,next_order) =1
+and DATEDIFF(day,next_order,next_order_2)=1;
