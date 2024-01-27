0edb5a85829d6a8045dcd9db585e47c9bc04e494
aanxniee <caishajia@gmail.com>
2024-01-27 18:07:21 -0500

    completes task1, task2, and task3

--- sql/task2.sql
+++ sql/task2.sql
@@ -2,18 +2,76 @@
 -- Write an SQL query to retrieve the products with the highest average rating.
 -- The result should include the product ID, product name, and the average rating.
 -- Hint: You may need to use subqueries or common table expressions (CTEs) to solve this problem.
+WITH
+    AverageRatings AS (
+        SELECT product_id, AVG(rating) AS avg_rating -- calculate the average rating
+        FROM Reviews
+        GROUP BY
+            product_id
+    )
+-- MAIN QUERY
+SELECT p.product_id, p.product_name, AR.avg_rating
+FROM Products p
+    JOIN AverageRatings AR ON p.product_id = AR.product_id
+WHERE
+    AR.avg_rating = (
+        SELECT MAX(avg_rating)
+        FROM AverageRatings -- retrive only the products with highest average rating
+    )
+
 
 -- Problem 6: Retrieve the users who have made at least one order in each category
 -- Write an SQL query to retrieve the users who have made at least one order in each category.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or joins to solve this problem.
+SELECT u.user_id, u.username
+FROM
+    Users u
+    -- 3 joins to get the products each user has bought
+    JOIN Orders o ON u.user_id = o.user_id
+    JOIN Order_Items oi ON o.order_id = oi.order_id
+    JOIN Products p ON oi.product_id = p.product_id
+GROUP BY
+    u.user_id
+HAVING
+    COUNT(DISTINCT p.category_id) = (
+        SELECT COUNT(*)
+        FROM Categories
+    ); -- filters for when a user has bought as many products in distinct categories as the total number of categories
+
 
 -- Problem 7: Retrieve the products that have not received any reviews
 -- Write an SQL query to retrieve the products that have not received any reviews.
 -- The result should include the product ID and product name.
 -- Hint: You may need to use subqueries or left joins to solve this problem.
+SELECT p.product_id, p.product_name
+FROM Products p
+    LEFT JOIN Reviews r ON p.product_id = r.product_id -- left join to reviews table on product id (includes all products)
+WHERE
+    r.review_id IS NULL -- filter for products with no reviews
+
 
 -- Problem 8: Retrieve the users who have made consecutive orders on consecutive days
 -- Write an SQL query to retrieve the users who have made consecutive orders on consecutive days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or window functions to solve this problem.
+WITH
+    ConsecutiveOrders AS (
+        SELECT
+            user_id, order_date, LAG(order_date) OVER (
+                PARTITION BY
+                    user_id
+                ORDER BY order_date
+            ) AS prev_order_date -- get the previous order date for each user
+        FROM Orders
+    )
+-- MAIN QUERY
+SELECT DISTINCT
+    u.user_id,
+    u.username
+FROM ConsecutiveOrders CO
+    JOIN Users u ON CO.user_id = u.user_id
+WHERE
+    order_date = DATE_ADD(
+        prev_order_date, INTERVAL 1 DAY
+    ); -- check for consecutive days
--- sql/task3.sql
+++ sql/task3.sql
@@ -2,18 +2,88 @@
 -- Write an SQL query to retrieve the top 3 categories with the highest total sales amount.
 -- The result should include the category ID, category name, and the total sales amount.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+SELECT c.category_id, c.category_name, SUM(oi.quantity * oi.unit_price) AS total_sales_amount -- calculate the total sales amount
+FROM
+    Categories c
+    JOIN Products p ON c.category_id = p.category_id
+    JOIN Order_Items oi ON p.product_id = oi.product_id
+GROUP BY
+    c.category_id,
+    c.category_name
+ORDER BY total_sales_amount DESC
+LIMIT 3; -- limit to the top 3 categories
+
 
 -- Problem 10: Retrieve the users who have placed orders for all products in the Toys & Games
 -- Write an SQL query to retrieve the users who have placed orders for all products in the Toys & Games
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+WITH
+    AllToyGameProducts AS (
+        -- CTE containing all toys & games products
+        SELECT *
+        FROM Products p
+            JOIN Categories c ON p.category_id = c.category_id
+        WHERE
+            c.category_name = 'Toys & Games'
+    )
+SELECT u.user_id, u.username
+FROM
+    Users u
+    JOIN Orders o ON u.user_id = o.user_id
+    JOIN Order_Items oi ON o.order_id = oi.order_id
+    JOIN Products p ON oi.product_id = p.product_id
+    JOIN Categories c ON p.category_id = c.category_id
+WHERE
+    c.category_name = 'Toys & Games' -- retrieves all toys & games products each user has bought
+GROUP BY
+    u.user_id
+    -- filters for the users where the number of toys & games products they have bought is equal to the total number of toys & games products
+HAVING
+    COUNT(DISTINCT p.product_id) = (
+        SELECT COUNT(*)
+        FROM AllToyGameProducts
+    );
+
 
 -- Problem 11: Retrieve the products that have the highest price within each category
 -- Write an SQL query to retrieve the products that have the highest price within each category.
 -- The result should include the product ID, product name, category ID, and price.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+WITH
+    ProductPriceRanking AS (
+        -- rank the products based on their price for each category
+        SELECT p.product_id, p.product_name, p.category_id, p.price, RANK() OVER (
+                PARTITION BY
+                    p.category_id
+                ORDER BY p.price DESC
+            ) as price_ranking
+        FROM Products p
+    )
+-- MAIN QUERY
+SELECT
+    product_id,
+    product_name,
+    category_id,
+    price
+FROM ProductPriceRanking
+WHERE
+    price_ranking = 1; -- get the top ranking product from each category
+
 
 -- Problem 12: Retrieve the users who have placed orders on consecutive days for at least 3 days
 -- Write an SQL query to retrieve the users who have placed orders on consecutive days for at least 3 days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+SELECT DISTINCT u.user_id, u.username FROM ( -- distinct to ensure unique users
+-- subquery to retrieve user_id and the next 3 order dates
+	SELECT o.user_id, o.order_date,
+        -- use lead to get the next_order_date and next_next_order_date
+		LEAD(order_date, 1) OVER (PARTITION BY user_id ORDER BY order_date) AS next_order_date,
+        LEAD(order_date, 2) OVER (PARTITION BY user_id ORDER BY order_date) AS next_next_order_date
+	FROM Orders o
+) AS sub_query
+JOIN Users u ON sub_query.user_id = u.user_id
+-- include only users who have made consecutive orders on 3 consecutive days
+WHERE sub_query.order_date = sub_query.next_order_date - INTERVAL '1 day'
+AND sub_query.order_date = sub_query.next_next_order_date - INTERVAL '2 days';
