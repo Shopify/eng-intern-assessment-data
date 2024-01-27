e8313df8cb55205dc8fe642ea225794cf5c86269
mirapatel19 <157907489+mirapatel19@users.noreply.github.com>
2024-01-27 12:17:45 -0500

    Update task1.sql

--- sql/task1.sql
+++ sql/task1.sql
@@ -15,11 +15,48 @@ HAVING category_cnt > 2
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
-
+SELECT CID,
+       COUNT(Orders.OrderID) AS TotalOrders,
+       SUM(OrderAmounts.DollarAmount) AS TotalDollarAmount
+FROM [Orders]
+INNER JOIN (SELECT OrderID, Sum(Quantity*SalePrice) AS DollarAmount 
+      FROM OrderItems GROUP BY OrderID) AS OrderAmounts
+  ON Orders.OrderID = OrderAmounts.OrderID
+GROUP BY CID
+ORDER BY Count(Orders.OrderID) DESC
+    
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
 -- The result should include the product ID, product name, and the average rating.
+select *
+ from PRODUCT p
+  join (
+    select
+      P_NUM,
+      avg(RATE) avg
+     from REVIEW
+     group by P_NUM
+     having sum( CASE WHEN RATE < 3 THEN 1
+                  ELSE 0
+                  END ) > ( count(*) * 0.5 )
+    ) as r
+   on p.P_NUM = r.P_NUM
 
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
+SELECT CUSTOMER_NAME, Y.QNTY 
+FROM CUSTOMER_T CUST,
+(
+  SELECT X.CUSTOMER_ID, X.QNTY, MAX(X.QNTY) MAXAMT
+  FROM (
+        SELECT ORD.CUSTOMER_ID, SUM(OLN.QUANTITY * PRD.UNIT_PRICE) QNTY
+        FROM ORDER_T ORD, ORDER_LINE_T OLN, PRODUCT_T PRD
+        WHERE TRUNC(OLN.ORDER_DATE,'YEAR') = TRUNC(SYSDATE,'YEAR')
+        AND ORD.ORDER_ID = OLN.ORDER_ID
+        AND PRD.PRODUCT_ID = OLN.PRODUCT_ID
+        GROUP BY ORD.CUSTOMER_ID
+       ) X 
+) Y
+WHERE CUST.CUSTOMER_ID = Y.CUSTOMR_ID
+AND Y.QNTY = Y.MAXAMT;
