eedabc1958d6b1327f2a93b586a937715c96e9c8
Zarina Dossayeva <zarina.dossayeva@gmail.com>
2024-01-29 04:51:01 +0600

    Dear Shopify Team, Thank you for the opportunity! I've addressed most problems, however could not resolve problems 7, 11 and 12.

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,14 +1,26 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
+SELECT p.* FROM products p
+INNER JOIN categories c ON p.category_id=c.category_id
+WHERE c.category_name='Sports & Outdoors';
 
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
+SELECT u.user_id, u.username, count(o.order_id) FROM orders o
+INNER JOIN users u ON u.user_id=o.user_id
+GROUP BY u.user_id, u.username;
 
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
 -- The result should include the product ID, product name, and the average rating.
+SELECT p.product_id, p.product_name, avg(r.rating) FROM products p
+INNER JOIN reviews r on p.product_id = r.product_id
+GROUP BY p.product_id, p.product_name;
 
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
+SELECT u.user_id, u.username, o.total_amount FROM users u
+INNER JOIN orders o on u.user_id = o.user_id
+ORDER BY o.total_amount DESC LIMIT 5;
--- sql/task3.sql
+++ sql/task3.sql
@@ -2,18 +2,39 @@
 -- Write an SQL query to retrieve the top 3 categories with the highest total sales amount.
 -- The result should include the category ID, category name, and the total sales amount.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+SELECT c.category_id, c.category_name, sum(o.total_amount) FROM categories c
+INNER JOIN products p on c.category_id = p.category_id
+INNER JOIN order_items oi on p.product_id = oi.product_id
+INNER JOIN orders o ON oi.order_id = o.order_id
+GROUP BY c.category_id, c.category_name
+ORDER BY sum(o.total_amount) DESC LIMIT 3;
+
 
 -- Problem 10: Retrieve the users who have placed orders for all products in the Toys & Games
 -- Write an SQL query to retrieve the users who have placed orders for all products in the Toys & Games
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+SELECT u.user_id, u.username FROM users u
+INNER JOIN orders o on u.user_id = o.user_id
+INNER JOIN order_items oi on oi.order_id=o.order_id
+INNER JOIN products p on p.product_id=oi.product_id
+INNER JOIN categories c on p.category_id = c.category_id
+WHERE c.category_name='Toys & Games'
+GROUP BY u.user_id, u.username
+HAVING (SELECT count( DISTINCT p.product_id)) = (SELECT COUNT(DISTINCT p1.product_id) FROM products p1
+    INNER JOIN categories c2 on c2.category_id = p1.category_id);
+
 
 -- Problem 11: Retrieve the products that have the highest price within each category
 -- Write an SQL query to retrieve the products that have the highest price within each category.
 -- The result should include the product ID, product name, category ID, and price.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
 
+--  ???
+
 -- Problem 12: Retrieve the users who have placed orders on consecutive days for at least 3 days
 -- Write an SQL query to retrieve the users who have placed orders on consecutive days for at least 3 days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+
+--- ???
