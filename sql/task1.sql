b752ba487dd2489f151cd61365edb020393fcd05
Janice Xu <59774704+janicexu16@users.noreply.github.com>
2024-01-28 21:57:24 -0500

    working queries and test

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,14 +1,34 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
+SELECT product_id, product_name
+FROM products
+WHERE category_id = (SELECT category_id FROM categories WHERE category_name LIKE '%Sports%');
 
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
 
--- Problem 3: Retrieve the average rating for each product
--- Write an SQL query to retrieve the average rating for each product.
--- The result should include the product ID, product name, and the average rating.
+SELECT user_id, username, COUNT(order_id) AS num_orders
+FROM orders 
+JOIN users USING (user_id)
+GROUP BY 1;
 
--- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
--- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
--- The result should include the user ID, username, and the total amount spent.
+-- -- Problem 3: Retrieve the average rating for each product
+-- -- Write an SQL query to retrieve the average rating for each product.
+-- -- The result should include the product ID, product name, and the average rating.
+
+SELECT product_id, product_name, AVG(rating)
+FROM products
+JOIN reviews USING (product_id)
+GROUP BY 1,2;
+
+-- -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
+-- -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
+-- -- The result should include the user ID, username, and the total amount spent.
+
+SELECT user_id, username, SUM(total_amount) AS total_amount_spent
+FROM orders
+JOIN users USING (user_id)
+GROUP BY 1,2
+ORDER BY total_amount_spent DESC
+LIMIT 5;
