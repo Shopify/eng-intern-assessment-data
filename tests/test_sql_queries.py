03903fa96ff669f23014fcffc6a1e5d17c71823e
AyawnKhan <123036810+AyawnKhan@users.noreply.github.com>
2024-01-28 23:37:50 -0600

    Updated Tests

--- tests/test_sql_queries.py
+++ tests/test_sql_queries.py
@@ -1,5 +1,8 @@
 import unittest
+import os
+import pandas as pd
 import psycopg2 # Replace with appropriate database connector based on your database
+from psycopg2 import extras
 
 # Instructions for running tests
 # 1. Build the Docker image: `docker build -f Dockerfile -t ayanstests .`
@@ -27,16 +30,47 @@ class TestSQLQueries(unittest.TestCase):
         with open('sql/schema.sql', 'r') as file:
             sql_code = file.read()
         self.cur.execute(sql_code)
+        self.conn.commit()
 
+    def read_data(self):
+        # List of table names in schema; CSVs are read in a specific order to not violate foreign key constraints
+        table_names = [
+            'Categories','Products', 'Users', 'Orders',
+            'Order_Items','Reviews','Cart','Cart_Items',
+            'Payments','Shipping'
+        ]
+        # List of file names corresponding to tables in schema
+        file_names = [
+            'category_data.csv','product_data.csv','user_data.csv','order_data.csv',
+            'order_items_data.csv', 'review_data.csv','cart_data.csv','cart_item_data.csv',
+            'payment_data.csv','shipping_data.csv'
+        ]
+
+        # Adds data to the database from all data files
+        for table, file in zip(table_names, file_names):
+            # Skips any data that causes a foreign key exception
+            try:
+                file_path = 'data/' + file
+                data = pd.read_csv(file_path)
+                columns = list(data.columns)
+                values = data.values.tolist()
+                print(table + ', ' + file)
+                query =  f"INSERT INTO {table} ({', '.join(columns)}) VALUES %s"
+                extras.execute_values(self.cur, query, values)
+            except psycopg2.DatabaseError as e:
+                self.conn.rollback()
+        self.conn.commit()
 
     def test_task1(self):
         self.read_schema()
+        self.read_data()
         # Task 1: Example SQL query in task1.sql
         with open('sql/task1.sql', 'r') as file:
             sql_query = file.read()
-
+        
         self.cur.execute(sql_query)
         result = self.cur.fetchall()
+        print(result)
 
         # Define expected outcome for Task 1 and compare
         expected_result = [
@@ -46,13 +80,13 @@ class TestSQLQueries(unittest.TestCase):
         self.assertEqual(result, expected_result, "Task 1: Query output doesn't match expected result.")
 
     def test_task2(self):
-        self.read_schema()
         # Task 2: Example SQL query in task2.sql
         with open('sql/task2.sql', 'r') as file:
             sql_query = file.read()
 
         self.cur.execute(sql_query)
         result = self.cur.fetchall()
+        print(result)
 
         # Define expected outcome for Task 2 and compare
         expected_result = [
@@ -62,15 +96,14 @@ class TestSQLQueries(unittest.TestCase):
         self.assertEqual(result, expected_result, "Task 2: Query output doesn't match expected result.")
     
     def test_task3(self):
-        self.read_schema()
-        # Task 1: Example SQL query in task1.sql
+        # Task 3: Example SQL query in task3.sql
         with open('sql/task3.sql', 'r') as file:
             sql_query = file.read()
 
         self.cur.execute(sql_query)
         result = self.cur.fetchall()
-
-        # Define expected outcome for Task 1 and compare
+        print(result)
+        # Define expected outcome for Task 3 and compare
         expected_result = [
             # Define expected rows or values here based on the query output
         ]
