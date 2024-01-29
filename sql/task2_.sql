bd5e19b483de374fb2a34c73f159c1a036075d6a
AkashGanesh777 <150173425+AkashGanesh777@users.noreply.github.com>
2024-01-28 21:04:06 -0500

    Update task2_.sql

--- sql/task2_.sql
+++ sql/task2_.sql
@@ -1,4 +1,7 @@
 --Problem 5
+--Returning a table with product id, product name and average rating of product
+--Joining the product_data table and review_data table with primary key product_id
+--Getting the top 5 ratings after listing it in descending order
 SELECT p.product_id, p.product_name, AVG(r.rating) AS average_rating
 FROM review_data r
 JOIN product_data p ON p.product_id = r.product_id
@@ -7,6 +10,10 @@ ORDER BY average_rating DESC
 LIMIT 5;
 
 --Problem 6
+--Selecting id and username of users from user_data tables
+--Joining the order_data table and user_data table with primary key user_id
+--Second join is to join the order_items_data and order_data with their primary key order_id
+--This will return users who made one purchase in each category
 SELECT u.user_id, u.username
 FROM user_data u
 JOIN order_data o ON u.user_id = o.user_id
@@ -14,6 +21,9 @@ JOIN order_items_data oi ON o.order_id = oi.order_id
 GROUP BY u.user_id, u.username;
 
 --Problem 7
+--Selecting id and name of products from product_data tables
+--Joining the review_data table and product_data table with primary key product_id
+--Returning product_id and name when rating and review text is null
 SELECT p.product_id, p.product_name
 FROM product_data p
 LEFT JOIN review_data r ON p.product_id = r.product_id
