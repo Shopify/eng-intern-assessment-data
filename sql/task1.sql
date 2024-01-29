9d381b0f17acfb8cbacf2d477c2f651b227ae54b
valeriewyso <valeriewyso@gmail.com>
2024-01-29 12:38:14 -0500

    Updated comments on task 1

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,5 +1,8 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
+
+-- Select the name from products to be outputted. Category id is related in Categories and Products so join them
+-- Looking for the category id that is equal to 8
 SELECT 
     P.product_name
 FROM 
@@ -9,9 +12,13 @@ INNER JOIN
 WHERE 
     C.category_id = 8;
 
+
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
+
+--Need User ID, Username and keep count of the number of orders for each user ID
+-- Joining user_id in both orders and users
 SELECT
     U.user_id,
     U.username,
@@ -27,6 +34,9 @@ GROUP BY
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
 -- The result should include the product ID, product name, and the average rating.
+
+--Need to Average the ratings
+--Join the product_id from reviews to products
 SELECT
     P.product_id,
     P.product_name,
@@ -42,6 +52,10 @@ GROUP BY
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
 
+--LIMIT 5 at the end for the top 5 users
+--Need the SUM of the total amount in Orders
+--Join user_id from users and orders to have corresponding users to orders
+--Filter the amount spent in a descending order from most spent to least amount spent
 SELECT
     U.user_id,
     U.username,
