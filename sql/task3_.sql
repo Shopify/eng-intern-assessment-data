7b3d942e39931aa0deaa42bd92e7a7d8209fc98e
AkashGanesh777 <150173425+AkashGanesh777@users.noreply.github.com>
2024-01-28 21:20:24 -0500

    Update task3_.sql

--- sql/task3_.sql
+++ sql/task3_.sql
@@ -1,3 +1,18 @@
+--Problem 9
+SELECT c.category_id, c.category_name, SUM(
+FROM user_data u
+JOIN order_data o1 on u.user_id = o1.user_id
+JOIN order_data o2 on u.user_id = o2.user_id AND o2.order_date = DATE_ADD(o1.order_date, INTERVAL 1 DAY)
+JOIN order_data o3 on u.user_id = o3.user_id AND o3.order_date = DATE_ADD(o1.order_date, INTERVAL 2 DAY)
+GROUP BY u.user_id, u.username
+LIMIT 3;
+
+--Problem 10
+
+
+--Problem 11
+
+
 --Problem 12
 --Selecting id and username of users from user_data tables
 --Joining the order_data table and user_data table with primary key user_id
