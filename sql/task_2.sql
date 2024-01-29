a1204a6e5fdb66b974967b3bb77f3ca2fc5e0edc
Adig930 <151580530+Adig930@users.noreply.github.com>
2024-01-29 11:15:11 -0500

    Update task_2.sql

--- sql/task_2.sql
+++ sql/task_2.sql
@@ -1,1 +1,10 @@
+-- Problem 5: Retrieve the products with the highest average rating
 
+
+-- Problem 6: Retrieve the users who have made at least one order in each category
+
+
+-- Problem 7: Retrieve the products that have not received any reviews
+
+
+-- Problem 8: Retrieve the users who have made consecutive orders on consecutive days
