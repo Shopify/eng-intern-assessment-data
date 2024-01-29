938b02ff55c5d47e5c6e969ea7229fd4b8c4e202
Adig930 <151580530+Adig930@users.noreply.github.com>
2024-01-29 18:48:09 -0500

    Update task_2.sql

--- sql/task_2.sql
+++ sql/task_2.sql
@@ -3,7 +3,7 @@ SELECT pd.product_id, pd.product_name, AVG(rd.rating) AS average_rating FROM pro
 JOIN review_data rd ON pd.product_id = rd.product_id
 GROUP BY pd.product_id, pd.product_name
 ORDER BY average_rating DESC
-LIMIT 5
+LIMIT 5;
 
 -- Problem 6: Retrieve the users who have made at least one order in each category
 SELECT ud.user_id, ud.username FROM user_data ud
@@ -14,8 +14,11 @@ GROUP BY ud.user_id, ud.username
 -- Problem 7: Retrieve the products that have not received any reviews
 SELECT pd.product_id, pd.product_name FROM product_data pd
 JOIN review_data rd ON pd.product_id = rd.product_id
-WHERE rd.review_text IS NULL AND rd.rating IS NULL
+WHERE rd.review_text IS NULL AND rd.rating IS NULL;
 
 -- Problem 8: Retrieve the users who have made consecutive orders on consecutive days
 SELECT ud.user_id, ud.username FROM user_data ud
-JOIN 
+JOIN order_data od ON ud.user_id = od.user_id
+JOIN order_data odtwo ON ud.user_id = odtwo.user_id AND odtwo.order_date = DATE_ADD(od.order_date, INTERVAL 1 DAY)
+GROUP BY ud.user_id, ud.username
+HAVING COUNT(DISTINCT od.order_id) > 1;
