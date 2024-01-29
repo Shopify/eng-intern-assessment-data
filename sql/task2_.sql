8ee773cf609aa21398afd28891dda267a2386e2b
AkashGanesh777 <150173425+AkashGanesh777@users.noreply.github.com>
2024-01-28 19:58:32 -0500

    Update task2_.sql

--- sql/task2_.sql
+++ sql/task2_.sql
@@ -1,1 +1,7 @@
-
+--Problem 5
+SELECT p.product_id, p.product_name, AVG(r.rating) AS average_rating
+FROM review_data r
+JOIN product_data p ON p.product_id = r.product_id
+GROUP BY p.product_id, p.product_name
+ORDER BY average_rating DESC
+LIMIT 5;
