3b66722c7da0d9b40f02c04e9ad81a733c36422d
William Sun <wcswill98@gmail.com>
2024-01-29 15:34:41 +0000

    shopify SQL problems completed

--- sql/task1.sql
+++ sql/task1.sql
@@ -12,10 +12,12 @@ WHERE category_id = 8
 -- The result should include the user ID, username, and the total number of orders.
 
 -- count order by grouping on user
-SELECT O.user_id, username, count(order_id) as total_orders
+SELECT 
+    O.user_id, username, 
+    count(order_id) as total_orders
 FROM Orders O
 LEFT JOIN Users U on O.user_id = U.user_id
-GROUP BY user_id
+GROUP BY O.user_id, username
 
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
@@ -25,16 +27,18 @@ GROUP BY user_id
 SELECT R.product_id, product_name, AVG(rating) as average_rating
 FROM Reviews R
 LEFT JOIN Products P on R.product_id = P.product_id
-GROUP BY R.product_id
+GROUP BY R.product_id, product_name
 
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
 
 -- Get top 5 total_spent by grouping on user, ordering desc, and limiting to 5
-SELECT O.user_id, username, SUM(total_amount) as total_spent
+SELECT TOP 5 
+    O.user_id, 
+    username, 
+    SUM(total_amount) as total_spent
 FROM Orders O
 LEFT JOIN Users U ON O.user_id = U.user_id
-GROUP BY O.user_id
-ORDER BY total_spent DESC 
-LIMIT 5
+GROUP BY O.user_id, username
+ORDER BY total_spent DESC;
