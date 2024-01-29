19afd043cc6c17ab1e4400dc71809d1299609c76
richard-dh-kim <58959649+richard-dh-kim@users.noreply.github.com>
2024-01-29 00:13:29 -0500

    Update config.py

--- tests/config.py
+++ tests/config.py
@@ -1,7 +1,7 @@
 DB_CONFIG = {
-    'dbname': 'postgres',
-    'user': 'postgres',
-    'password': '1390',
-    'host': 'localhost',
-    'port': '5432'
+    'dbname': '',
+    'user': '',
+    'password': '',
+    'host': '',
+    'port': ''
 }
