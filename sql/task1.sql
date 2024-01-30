8826708a9c4e391b4fb2f1767e7da515e321e9d9
Aidan Saull <saull.aidan@gmail.com>
2024-01-29 20:27:40 -0500

    Completed all problems in tasks

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,14 +1,66 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
 
+select * from products
+where category_id=8;
+
+-- Alternatively this will allow you to search by a string by joining with the categories table
+
+select * from categories join products on categories.category_id = products.category_id
+where category_name = 'Sports & Outdoors';
+
+
+
+
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
--- The result should include the user ID, username, and the total number of orders.
+-- The result should include the user ID, username, and the total number of orders
+
+-- We group by user id to see how many orders there are per id
+-- Note that to test this, it was necessary to change the data so that there was
+-- at least one user with no orders, and at least one with more than 1 order.
+-- Going forward the data was modified several times for testing
+
+select users.user_id, users.username, count(orders.order_id) num_orders
+from users left join orders on users.user_id = orders.user_id
+group by users.user_id
+-- The order is changed, so this fixes that
+order by users.user_id;
+
+
+
 
 -- Problem 3: Retrieve the average rating for each product
 -- Write an SQL query to retrieve the average rating for each product.
 -- The result should include the product ID, product name, and the average rating.
 
+-- This is very similar the the previous one, we use average instead of count, and we
+-- join reviews and products, with products on the left.
+
+select products.product_id, products.product_name, avg(reviews.rating) avg_review
+from products left join reviews on products.product_id = reviews.product_id
+group by products.product_id
+order by products.product_id;
+
+
+
+
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
+
+-- Again this is very similar the the last one, this time we are summing the total order
+-- amount, and sorting by it, then taking the top 5
+
+select users.user_id, users.username, sum(orders.total_amount) total_spent from
+users left join orders on users.user_id = orders.user_id
+group by users.user_id
+-- Note that we must put nulls last, otherwise they will be on top
+order by total_spent desc nulls last
+limit 5;
+
+-- Also note that users with the same total spending may be sorted arbitrarily here,
+-- so they may get cut off even though they are technically in the top 5.
+-- We could use a WHERE statement and a comparison to determine which users have at least the amount
+-- spent as the 5th highest spender, and list all of them, but I 
+-- figure the intent of the query is to only return 5 users.
--- sql/task2.sql
+++ sql/task2.sql
@@ -3,17 +3,91 @@
 -- The result should include the product ID, product name, and the average rating.
 -- Hint: You may need to use subqueries or common table expressions (CTEs) to solve this problem.
 
+-- We use a CTE of the previous problem so that we can reference it twice
+
+with avg_reviews as
+(
+    -- This is a table of the average reviews of each product
+	select products.product_id, products.product_name, avg(reviews.rating) avg_review
+	from products left join reviews on products.product_id = reviews.product_id
+	group by products.product_id
+	order by avg_review desc nulls last
+)
+-- Then we take all rows for which the average review is equal to the maximum average review
+select * from avg_reviews
+where avg_reviews.avg_review = 
+(
+	select max(avg_review) from avg_reviews
+);
+
+
+
+
 -- Problem 6: Retrieve the users who have made at least one order in each category
 -- Write an SQL query to retrieve the users who have made at least one order in each category.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or joins to solve this problem.
+
+-- I wasn't 100% sure what the question meant by one order in each category, I decided to retrieve
+-- all users that had ordered at least one item in each category.
+-- Although I suppose it could mean all users which had, for every category, a distinct order with an
+-- item of that category in that order.
+
+-- I assume that category_id is distinct
+
+
+select users.user_id, users.username from
+(
+    -- By left joining onto order_items, we get a table of all items ordered, which we then
+    -- group by user_id to count the number of distinct product categories for each user.
+	select orders.user_id, count(distinct products.category_id) num from order_items
+	left join products on products.product_id = order_items.product_id
+	left join orders on orders.order_id = order_items.order_id
+	group by orders.user_id
+) as unique_counts
+-- We now have the counts for each user, so we can see which ones
+-- have a count equal to the total number of categories
+left join users on users.user_id = unique_counts.user_id
+where unique_counts.num = 
+(
+    select count (category_id) from categories
+);
+
+
+
 
 -- Problem 7: Retrieve the products that have not received any reviews
 -- Write an SQL query to retrieve the products that have not received any reviews.
 -- The result should include the product ID and product name.
 -- Hint: You may need to use subqueries or left joins to solve this problem.
 
+-- This one is quite straightforward. On a left join with reviews we will have
+-- null values if there are no reviews to join with.
+
+select products.product_id, products.product_name from products
+left join reviews on products.product_id = reviews.product_id
+where review_id is null;
+
+
+
+
 -- Problem 8: Retrieve the users who have made consecutive orders on consecutive days
 -- Write an SQL query to retrieve the users who have made consecutive orders on consecutive days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or window functions to solve this problem.
+
+-- We count the number of days between each order for each user and then
+-- select the users which have at least one order that directly proceeded the
+-- previous one.
+
+select distinct users.user_id, users.username from 
+users left join
+(
+	-- This subquery counts days between orders for each order. Since it is partitioned by user, we
+	-- do not include cases where user 4 ordered one day after user 3
+	select orders.order_id, orders.user_id, orders.order_date, orders.order_date - lag(orders.order_date)
+	over (partition by orders.user_id order by orders.order_date) as days_between
+	from orders
+) as count_days
+on count_days.user_id = users.user_id
+where count_days.days_between = 1;
