ebde1620172bc4e2aa90038609e4af489026ba00
aryanbagga <aryanbagga8@gmail.com>
2024-01-29 21:33:08 -0500

    solutions added

--- sql/task2.sql
+++ sql/task2.sql
@@ -3,17 +3,53 @@
 -- The result should include the product ID, product name, and the average rating.
 -- Hint: You may need to use subqueries or common table expressions (CTEs) to solve this problem.
 
+WITH AVG_RATING AS (
+     SELECT product_id, AVG(rating) as average_rating
+     FROM Reviews
+     GROUP BY product_id
+ )
+ SELECT p.product_id, p.product_name, a.average_rating 
+ FROM Products p
+ INNER JOIN AVG_RATING a
+ ON p.product_id = a.product_id
+ ORDER BY a.average_rating;
+ -- Return a list of all products with their average rating in descending order of average rating. If the question is asking
+ -- for only products with the highest average rating, then you can use
+ -- "WHERE a.average_rating = (SELECT MAX(avg_rating) FROM AVG_RATING)"
+
+
 -- Problem 6: Retrieve the users who have made at least one order in each category
 -- Write an SQL query to retrieve the users who have made at least one order in each category.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or joins to solve this problem.
+
+SELECT u.user_id, u.username
+ FROM Users u
+ INNER JOIN Orders o ON u.user_id = o.user_id
+ INNER JOIN Order_Items oi ON o.order_id = oi.order_id
+ INNER JOIN Products p ON oi.product_id = p.product_id
+ GROUP BY u.user_id, u.username
+ HAVING COUNT(DISTINCT p.category_id) = (SELECT COUNT(*) FROM Categories);
 
 -- Problem 7: Retrieve the products that have not received any reviews
 -- Write an SQL query to retrieve the products that have not received any reviews.
 -- The result should include the product ID and product name.
 -- Hint: You may need to use subqueries or left joins to solve this problem.
 
+SELECT p.product_id, p.product_name
+ FROM Products p
+ WHERE NOT EXISTS 
+     (SELECT * 
+     FROM Reviews r
+     WHERE p.product_id = r.product_id);
+
 -- Problem 8: Retrieve the users who have made consecutive orders on consecutive days
 -- Write an SQL query to retrieve the users who have made consecutive orders on consecutive days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or window functions to solve this problem.
+
+SELECT DISTINCT u.user_id, u.username
+ FROM Users u 
+ INNER JOIN Orders o ON u.user_id = o.user_id
+ INNER JOIN Orders oo ON u.user_id = oo.user_id
+ WHERE DATEDIFF(day, o.order_date, oo.order_date) = 1;
--- sql/task3.sql
+++ sql/task3.sql
@@ -3,17 +3,58 @@
 -- The result should include the category ID, category name, and the total sales amount.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
 
+SELECT c.category_id, c.category_name, sum(oi.quantity * oi.unit_price) as total_sales_amount
+ FROM Categories c 
+ INNER JOIN Products p ON c.category_id = p.category_id
+ INNER JOIN Order_Items oi ON p.product_id = oi.product_id
+ GROUP BY c.category_id, c.category_name
+ ORDER BY total_sales_amount DESC
+ LIMIT 3;
+
 -- Problem 10: Retrieve the users who have placed orders for all products in the Toys & Games
 -- Write an SQL query to retrieve the users who have placed orders for all products in the Toys & Games
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and aggregate functions to solve this problem.
+
+SELECT DISTINCT u.user_id, u.username
+ FROM Users u
+ INNER JOIN Orders o ON u.user_id = o.user_id
+ INNER JOIN Order_Items oi ON o.order_id = oi.order_id
+ INNER JOIN Products p ON oi.product_id = p.product_id
+ INNER JOIN Categories c ON p.category_id = c.category_id
+ WHERE c.category_name = 'Toys & Games'
+ GROUP BY u.user_id, u.username
+ HAVING COUNT(DISTINCT p.product_id) = (
+     SELECT COUNT(*) FROM Products p_2
+     INNER JOIN categories c_2 ON p_2.category_id = c_2.category_id
+     WHERE c_2.category_name = 'Toys & Games'
+ )
 
 -- Problem 11: Retrieve the products that have the highest price within each category
 -- Write an SQL query to retrieve the products that have the highest price within each category.
 -- The result should include the product ID, product name, category ID, and price.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
 
+WITH HIGHEST_PRICE AS (
+     SELECT product_id, product_name, category_id, price,
+     ROW_NUMBER() OVER (PARTITION BY category_id ORDER BY price DESC) AS row_num
+     FROM products
+ )
+ SELECT product_id, product_name, category_id, price
+ FROM HIGHEST_PRICE
+ WHERE row_num = 1;
+
+
 -- Problem 12: Retrieve the users who have placed orders on consecutive days for at least 3 days
 -- Write an SQL query to retrieve the users who have placed orders on consecutive days for at least 3 days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+
+SELECT DISTINCT u.user_id, u.username
+ FROM Users u 
+ INNER JOIN Orders o on u.user_id = o.user_id
+ INNER JOIN Orders oo on u.user_id = oo.user_id
+ INNER JOIN Orders ooo on u.user_id = ooo.user_id
+ WHERE DATEDIFF(day, o.order_date, oo.order_date) = 1
+         AND DATEDIFF(day, oo.order_date, ooo.order_date) = 1
+         AND DATEDIFF(day, o.order_date, ooo.order_date) = 2;
