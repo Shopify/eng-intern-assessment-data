6e724f39c58692facd5797a2858db9889caeb207
harshita-sankar <139141341+harshita-sankar@users.noreply.github.com>
2024-01-29 17:39:33 -0800

    tasks 1,2,3 completed

--- sql/task1.sql
+++ sql/task1.sql
@@ -12,3 +12,30 @@
 -- Problem 4: Retrieve the top 5 users with the highest total amount spent on orders
 -- Write an SQL query to retrieve the top 5 users with the highest total amount spent on orders.
 -- The result should include the user ID, username, and the total amount spent.
+
+-- Problem 1 Solution:
+
+SELECT product_name FROM product_data WHERE category_id = 8;
+
+-- Problem 2 Solution:
+
+SELECT cart_data.user_id, user_data.username, SUM(cart_item_data.quantity) AS total_orders
+FROM cart_data 
+INNER JOIN user_data ON cart_data.user_id = user_date.user_id;
+INNER JOIN cart_item_data ON cart_data.cart_id = cart_item_data.cart_id;
+
+-- Problem 3 Solution:
+
+SELECT review_data.product_id, product_data.product_name, (
+    SELECT AVG(review_data.rating) 
+    FROM review_data 
+    WHERE review_data.product_id = product_data.product_id
+) AS average_rating
+FROM product_data;
+
+-- Problem 4 Solution:
+
+SELECT TOP 5 order_data.user_id, user_data.username, order_data.total_amount
+FROM order_data
+INNER JOIN user_data ON order_data.user_id = user_data.user_id
+ORDER BY order_data.total_amount DESC;
--- sql/task3.sql
+++ sql/task3.sql
@@ -17,3 +17,68 @@
 -- Write an SQL query to retrieve the users who have placed orders on consecutive days for at least 3 days.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries, joins, and window functions to solve this problem.
+
+
+-- Problem 9 Solution:
+
+WITH category_sales_data AS (
+    SELECT category_.category_id, category_data.category_name, SUM(order_data.total_amount) AS total_sales
+    FROM category_data
+    LEFT JOIN product_data ON category_data.category_id = product_data.category_id
+    LEFT JOIN order_items_data ON product_data.product_id = order_items_data.product_id
+    LEFT JOIN order_data ON order_items_data.order_id = order_data.order_id
+    GROUP BY category_data.category_id, category_data.category_name
+)
+
+SELECT TOP 3 category_id, category_name, total_sales
+FROM category_sales_data
+ORDER BY total_sales DESC;
+
+-- Problem 10 Solution:
+
+SELECT user_data.user_id, user_data.username
+FROM user_data
+WHERE NOT EXISTS (
+    SELECT product_data.product_id
+    FROM product_data
+    WHERE product_data.category_id = (
+            SELECT category_id
+            FROM category_data
+            WHERE category_name = 'Toys & Games'
+        )
+    EXCEPT 
+    SELECT product_data.product_id
+    FROM product_data
+    INNER JOIN order_items_data ON product_data.product_id = order_items.product_id
+    INNER JOIN order_data ON order_items_data.order_id = order_data.order_id
+    WHERE order_data.user_id = user_data.user_id
+);
+
+-- Problem 11 Solution:
+
+WITH max_price_per_category AS (
+    SELECT
+        product_data.product_id, product_data.product_name, product_data.category_id, product_data.price,
+        ROW_NUMBER() OVER (PARTITION BY product_data.category_id ORDER BY product_data.price DESC) AS row_num
+    FROM product_data
+)
+
+SELECT product_id, product_name, category_id, price
+FROM max_price_per_category
+WHERE row_num = 1;
+
+
+-- Problem 12 Solution:
+
+WITH user_consecutive_orders_data AS (
+    SELECT user_id, order_date,
+        LEAD(order_date, 1) OVER (PARTITION BY user_id ORDER BY order_date) AS next_order_date,
+        LEAD(order_date, 2) OVER (PARTITION BY user_id ORDER BY order_date) AS next_next_order_date
+    FROM order_data
+)
+
+SELECT DISTINCT user_data.user_id, user_data.username
+FROM user_data u
+JOIN user_consecutive_orders_data ON user_data.user_id = user_consecutive_orders_data.user_id
+WHERE user_consecutive_orders_data.next_order_date = DATEADD(day, 1, user_consecutive_orders_data.order_date)
+    AND user_consecutive_orders_data.next_next_order_date = DATEADD(day, 2, user_consecutive_orders_data.order_date);
