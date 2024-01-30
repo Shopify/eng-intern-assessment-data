a6e6302cbc522dbb5f72399e323ed766831313dc
Marwa Khalid <marwaa.khaliid@gmail.com>
2024-01-29 20:08:09 -0500

    Problem 1: Completed

--- sql/task1.sql
+++ sql/task1.sql
@@ -1,6 +1,17 @@
 -- Problem 1: Retrieve all products in the Sports category
 -- Write an SQL query to retrieve all products in a specific category.
 
+SELECT
+    P.product_id,
+    P.product_name,
+    P.description,
+    P.price,
+FROM
+    Products P,
+    Categories C
+WHERE
+    P.category_id = C.category_id AND C.category_name = 'Sports & Outdoors';
+
 -- Problem 2: Retrieve the total number of orders for each user
 -- Write an SQL query to retrieve the total number of orders for each user.
 -- The result should include the user ID, username, and the total number of orders.
