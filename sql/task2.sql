4c1531bdaaeef603f1e888b32da8ba4003cab220
a-linco <105692753+a-linco@users.noreply.github.com>
2024-01-29 23:19:49 -0500

    Problem 5 and 6 updated

--- sql/task2.sql
+++ sql/task2.sql
@@ -3,10 +3,51 @@
 -- The result should include the product ID, product name, and the average rating.
 -- Hint: You may need to use subqueries or common table expressions (CTEs) to solve this problem.
 
+WITH AvgRating AS (
+    SELECT 
+        products.product_id, products.product_name, AVG(rating) AS product_average_rating
+    FROM 
+        reviews
+    JOIN 
+        products ON products.product_id = reviews.product_id
+    GROUP BY 
+        products.product_id
+) -- This block of code creates the CTE
+
+SELECT 
+    product_id, product_name, product_average_rating
+FROM 
+    AvgRating
+WHERE 
+    product_average_rating = (SELECT MAX(product_average_rating) FROM AvgRating); -- get the max value from AvgRating CTE
+
 -- Problem 6: Retrieve the users who have made at least one order in each category
 -- Write an SQL query to retrieve the users who have made at least one order in each category.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or joins to solve this problem.
+
+SELECT 
+    username
+FROM 
+    users
+WHERE (
+    SELECT 
+        COUNT(DISTINCT category_id) 
+    FROM 
+        orders 
+    JOIN 
+        order_items ON order_items.order_id = orders.order_id 
+    JOIN 
+        products ON order_items.product_id = products.product_id
+    WHERE orders.user_id = users.user_id
+    )  = 
+    
+    (SELECT 
+        COUNT(DISTINCT category_id) -- check if the user has made at least one order in each category
+     FROM 
+        categories); 
+        
+	-- There are no users that have made at least one order in each category!
 
 -- Problem 7: Retrieve the products that have not received any reviews
 -- Write an SQL query to retrieve the products that have not received any reviews.
